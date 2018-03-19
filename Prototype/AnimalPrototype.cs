119b3281450d8ddf99d9c58b99567003c7cc5416
Anthony Hoopes (WTIA Workforce Institute) <v-dohoop@microsoft.com>
2018-03-19 15:05:38 -0700

    Prototype pattern

---
+++ Prototype/AnimalPrototype.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class AnimalPrototype : Cloneable
+    {
+        public int LegCount;
+        public string Name;
+
+        public AnimalPrototype (int legCount, string name)
+        {
+            LegCount = legCount;
+            Name = name;
+        }
+
+        public override Cloneable Clone()
+        {
+            Console.WriteLine($"Cloning {LegCount} legged, {Name}");
+            var clone = MemberwiseClone() as AnimalPrototype;
+            clone.LegCount = UpdateLegs();
+            clone.Name = clone.Name + "_2";
+            return clone;
+        }
+
+        public int UpdateLegs()
+        {
+            return LegCount + 2;
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Save passed clones
+            var cloneFarm = new Dictionary<string, AnimalPrototype>();
+
+            // Initialize some clones
+            cloneFarm["Animal_1"] = new AnimalPrototype(4, "Betsy");
+            cloneFarm["Animal_2"] = new AnimalPrototype(5, "Penta");
+            cloneFarm["Animal_3"] = new AnimalPrototype(3, "Accidenta");
+
+            // Clone some animals!
+            AnimalPrototype betsyClone = cloneFarm["Animal_1"].Clone() as AnimalPrototype;
+            AnimalPrototype ginaClone = cloneFarm["Animal_2"].Clone() as AnimalPrototype;
+            AnimalPrototype accidentaClone = cloneFarm["Animal_3"].Clone() as AnimalPrototype;
+
+            // Add clones to the clone farm
+            cloneFarm.Add("Animal_4", betsyClone);
+            cloneFarm.Add("Animal_5", ginaClone);
+            cloneFarm.Add("Animal_6", accidentaClone);
+
+            // Looks like we got some mutations
+            Console.WriteLine("\n");
+            Console.WriteLine("All the anmials in the clone farm are:");
+            foreach (var clone in cloneFarm)
+            {
+                Console.WriteLine($"{clone.Key} is {clone.Value.LegCount} legged, {clone.Value.Name}");
+            }
+        }
+
+        
+    }
+}
